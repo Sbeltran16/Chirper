cbf18e150df70c14a66e8e3661d1227fbc296996
Sbeltran16 <sbeltran16@gmail.com>
2022-04-08 15:56:22 -0700

    Create models for posts and users

---
+++ models/post.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+
+const commentSchema = new Schema({
+    userId: Schema.Types.ObjectId,
+    userText: String
+})
+
+
+
+
+const postSchema = new Schema({
+    userId: Schema.Types.ObjectId,
+    userText: String,
+    comment: [commentSchema]
+
+})
+
+
+module.exports = mongoose.model('Post', postSchema);
--- models/user.js
+++ models/user.js
@@ -1,3 +1,25 @@
 const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
 
 // Create your User Model
+
+const userSchema = new Schema({
+    userId: Schema.Types.ObjectId,
+    userName: String,
+    email : [emailSchema],
+    password: {
+        type: String,
+        required: true
+    }
+})
+
+const emailSchema = new Schema({
+    email:{
+        type: String,
+        lowercase: true,
+        unique: true,
+        required: 'Email address is required',
+    }
+})
+
+module.exports = mongoose.model('User', userSchema);
