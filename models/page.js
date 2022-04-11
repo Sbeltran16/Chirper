c3877ec2beca8fba017ce9ed403b0e90c7105757
Sbeltran16 <sbeltran16@gmail.com>
2022-04-11 16:26:24 -0700

    create "View Posts" button to take me to Post page

--- models/page.js
+++ models/page.js
@@ -2,9 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 
+const commentSchema = new Schema({
+    userId: Schema.Types.ObjectId,
+    userText: String
+})
+
 
 const postSchema = new Schema({
+    userId: Schema.Types.ObjectId,
     userText: String,
+    comment: [commentSchema]
+
 })
 
 
