9869476a737d8cc7b68c28433c4c85e626758048
Sbeltran16 <sbeltran16@gmail.com>
2022-04-14 16:55:37 -0700

    heroku deploy

--- routes/pages.js
+++ routes/pages.js
@@ -2,8 +2,8 @@ const express = require('express');
 const router = express.Router();
 const pagesCtrl = require('../controllers/pages');
 
-router.get('/create', pagesCtrl.new);
 router.get('/home', pagesCtrl.index);
+router.get('/create', pagesCtrl.new);
 router.post('/', pagesCtrl.create);
 router.get('/:id/post', pagesCtrl.show);
 router.delete('/:id', pagesCtrl.delete);
