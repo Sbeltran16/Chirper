18b86d831816ee357e91b60b7f4092612b5c03e1
Sbeltran16 <sbeltran16@gmail.com>
2022-04-14 19:53:35 -0700

    update comment model and home redirect

--- models/comment.js
+++ models/comment.js
@@ -5,7 +5,8 @@ const Schema = mongoose.Schema;
 
 const commentSchema = new mongoose.Schema({
     commentText: {type: String, required: true},
-    chirpId: {type: Schema.Types.ObjectId, ref: 'Chirp', required: true}
+    chirpId: {type: Schema.Types.ObjectId, ref: 'Chirp', required: true},
+    userId: {type: Schema.Types.ObjectId, ref: 'User', requried: true}
 }, {timestamps: true })
 
 module.exports = mongoose.model('Comment', commentSchema);
--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const passport = require('passport');
 
 // The root route renders our only view
 router.get('/', function(req, res) {
-    res.redirect('/Chirper')
+    res.redirect('/Chirper/home')
   // Where do you want to go for the root route
   // in the student demo this was res.redirect('/students'), what do you want?
   // This could be a landing page, or just redirect to your main resource page which you'll have an a tag that makes 
